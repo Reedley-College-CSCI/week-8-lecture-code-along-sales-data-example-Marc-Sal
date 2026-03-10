71aed193299604f4f2ed425d11853c5a2684fe4c
swagmasterteletubie@gmail.com <swagmasterteletubie@gmail.com>
2026-03-10 09:45:23 -0700

    declared array of doubles for sales and  reported the first value

--- Project1/salesdata.cpp
+++ Project1/salesdata.cpp
@@ -1,7 +1,16 @@
 //marcos saldana
-
+//Week 8 Demo
 #include <iostream>
 
-int main() {
+using namespace std;
 
+int main() {
+	const int MAX_SIZE = 10;
+	int numDays = 7;
+
+	double amounts[MAX_SIZE] = { 1200, 1800, 900, 1500, 800, 1100, 1300 };
+	cout << "Welcome to Store Sales Data Analysis! " << endl;
+	cout << "Day 1 sales: " << amounts[0] << endl;
+	return 0;
 }
+
