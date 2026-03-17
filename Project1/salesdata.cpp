e5dee45cf0f96872870174bc7efd652a5f0faa16
swagmasterteletubie@gmail.com <swagmasterteletubie@gmail.com>
2026-03-17 10:18:25 -0700

    added parralel arrays, and structs

--- Project1/salesdata.cpp
+++ Project1/salesdata.cpp
@@ -4,33 +4,62 @@
 
 using namespace std;
 
+struct Day {
+	double amount;
+	int transaction;
+};
+
 void showDay();
+void showAmounts(double arr[], int size);
+void showAmounts(Day arr[], int size);
 
 int main() {
 	const int MAX_SIZE = 10;
 	int numDays = 7;
+	double total = 0;
 
 	double amounts[MAX_SIZE] = { 1200, 1800, 900, 1500, 800, 1100, 1300 };
+	int max = amounts[0];
 
-
-	cout << "Welcome to Store Sales Data Analysis! " << endl;
-	cout << "Day 1 sales: " << amounts[0] << endl;
-	cout << "Day 2 sales: " << amounts[1] << endl;
-
-	/*
-	showDay();
-	showDay();
-
-	cout << "Arrray memory : ~ " << sizeof(amounts) << " bytes" << endl; */
+	int transactions[MAX_SIZE] = { 50, 75, 40, 60, 45, 55 };
 
 	for (int i = 0; i < numDays; i++) {
-		cout << "Day " << i + 1 << ": $" << amounts[i] << endl;
+		cout << "Day " << i + 1 << ": $" << amounts[i] << " , " << transactions[i] << endl;
+		total += amounts[i];
+		if (amounts[i] > max) max = amounts[1];
 	}
+	cout << "Total sales: $" << total << endl;
+	cout << "Average: $" << total / numDays << endl;
+	cout << "Highest Sales: $" << max << endl;
+
+	cout << "Sales via function" << endl;
+	showAmounts(amounts, numDays);
+
+	struct Day {
+		double amount;
+		int transaction;
+	};
+
+	Day sales[MAX_SIZE] = { {1200,50}, {1800,75}, {900,40}, {1500,60}, {800,30}, {1100,45}, {1300,55} };
+	cout << "Day 2 Sales: $" << sales[1].amount << endl;
 	return 0;
 }
 
 void showDay() {
 	static int  dayNum = 1; //Persists
 	cout << "Showing day" << dayNum << endl;
+}
+void showAmounts(double arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << "$" << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void showAmounts(Day arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << "$" << arr[i].amount << " ";
+	}
+	cout << endl;
 }
 
